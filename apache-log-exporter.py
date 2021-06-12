2f01d61a7c871ddbd9f466852b6d8d2ada3119d7
Scott Baker <smbaker@gmail.com>
2021-06-12 16:43:59 -0700

    working

--- apache-log-exporter.py
+++ apache-log-exporter.py
@@ -1,9 +1,20 @@
-from prometheus_client import Counter, Summary, start_http_server
+#
+# setup
+#   # note: installed promtheus_clien=0.11.0, apachelogs=0.6.0, pydicti=1.1.4
+#   sudo pip3 install promteheus_client
+#   sudo pip3 install apachelogs
+
+from prometheus_client import Histogram, Counter, Summary, start_http_server
+import argparse
 import os
 import stat
 import sys
 import time
-from apachelogs import LogParser, COMBINED
+import yaml
+from apachelogs import LogParser, COMBINED, COMBINED_DEBIAN, COMMON, COMMON_DEBIAN, VHOST_COMBINED, VHOST_COMMON
+
+
+UNSPECIFIED = "def"
 
 
 class InodeChangedError(Exception):
@@ -19,23 +30,25 @@ def GetInodeAndSize(fn):
     return (st[stat.ST_INO], st[stat.ST_SIZE])
 
 
-def follow(fn):
+def follow(fn, ignoreExisting=False):
     """ Yield each line from a file as they are written.
         Return InodeChangedError if the Inode changed.
         Return FileShrunkError if the file shrunk
     """
 
     origInode, lastSize = GetInodeAndSize(fn)
-    file = open("sample.log", 'r')
+    file = open(fn, 'r')
     line = ''
     while True:
         tmp = file.readline()
         if (tmp is not None) and (tmp != ""):
             line += tmp
             if line.endswith("\n"):
-                yield line
+                if (not ignoreExisting):
+                    yield line
                 line = ''
         else:
+            ignoreExisting = False
             time.sleep(1)
             newInode, newSize = GetInodeAndSize(fn)
             if (newInode != origInode):
@@ -48,40 +61,45 @@ def follow(fn):
 def warn(s):
     print(s, file=sys.stderr)
 
+
 class ApacheLogExporter:
-    def __init__(self, fn = "sample.log", port=9101):
+    def __init__(self, fn="sample.log", port=9101, resolver={}, format=VHOST_COMBINED, ignoreExisting=True, enableHistogram=True):
         self.fn = fn
-        self.parser = LogParser(COMBINED)
+        self.parser = LogParser(format)
+        self.resolver = resolver
+        self.ignoreExisting = ignoreExisting
+        self.enableHistogram = enableHistogram
 
-        self.webRequestCount = Counter('apache_web_request_count', 
+        # A summary is effectively a pair of counters, one incremented
+        # for each observe, and one added to for each observe.
+        self.webRequestSummary = Summary('apache_web_request', 
                                    'Requests processed by the web server',
-                                   ['remote_host_name', 'remote_host_port', 'final_status'])
+                                   ['virtual_host', 'server_port', 'final_status', "remote_host"])
 
-        self.webRequestSent = Counter('apache_web_request_sent', 
-                                   'Size of Requests processed by the web server',
-                                   ['remote_host_name', 'remote_host_port', 'final_status'])                                   
+        if self.enableHistogram:
+            # lots of buckets really increases the size of the scrape
+            buckets=(4096,8192,16384,32768,65536,131072,262144,524288,1048576)
+            self.webRequestBytesOutHistogram = Histogram('apache_web_request_bytes_out', 
+                                   'Histogram of request bytes_out',
+                                   ['virtual_host', 'remote_host'],
+                                   buckets=buckets)
 
         start_http_server(port)
 
     def parse_line(self, line):
         entry = self.parser.parse(line)
 
-        if not entry.remote_host:
-            warn("no remote_host in line: %s" % line)
-            return None
+        #print(entry.virtual_host, entry.server_port, entry.remote_host, entry.final_status, entry.bytes_out)
+        #print(dir(entry))
+        #sys.exit(-1)
 
-        if ":" not in entry.remote_host:
-            warn("no colon in remote_host in line: %s" % line)
-            return None
-
-        entry.remote_host_name, entry.remote_host_port = entry.remote_host.split(":",1)
-        entry.remote_host_port = int(entry.remote_host_port)
         return entry
 
     def read_log_files(self):
+        ignoreExisting = self.ignoreExisting
         while True:
             try:
-                for line in follow("sample.log"):
+                for line in follow(self.fn, ignoreExisting):
                     try:
                         entry = self.parse_line(line)
                     except Exception as e:
@@ -91,23 +109,92 @@ class ApacheLogExporter:
                     if not entry:
                         continue
 
-                    self.webRequestCount.labels(
-                        remote_host_name=entry.remote_host_name,
-                        remote_host_port=str(entry.remote_host_port),
-                        final_status = str(entry.final_status)).inc()
+                    remote_host = self.resolver.get(entry.remote_host, UNSPECIFIED)
 
-                    self.webRequestSent.labels(
-                        remote_host_name=entry.remote_host_name,
-                        remote_host_port=str(entry.remote_host_port),
-                        final_status = str(entry.final_status)).inc(entry.bytes_sent)                        
+                    self.webRequestSummary.labels(
+                        virtual_host=entry.virtual_host,
+                        server_port=str(entry.server_port),
+                        final_status = str(entry.final_status),
+                        remote_host = remote_host).observe(entry.bytes_out)
 
-                    print(entry.remote_host_name, entry.remote_host_port, entry.final_status, entry.bytes_sent, entry.request_line)
+                    if self.enableHistogram:
+                        self.webRequestBytesOutHistogram.labels(
+                            virtual_host=entry.virtual_host,
+                            remote_host = remote_host).observe(entry.bytes_out)                        
             except (FileNotFoundError, InodeChangedError, FileShrunkError) as e:
+                print("exception: %s", e)
+                ignoreExisting = False
                 time.sleep(1)        
 
 
+def getParamDefault(yml, name, default):
+    parts = name.split(".")
+    for part in parts:
+        if part not in yml:
+            return default
+        yml = yml[part]
+    return yml
+
+
+def parseBool(s):
+    if (s==True) or (s==False):
+        return s
+    if (s.lower()=="true") or (s.lower()=="on") or (s.lower()=="yes"):
+        return True
+    if (s=="1"):
+        return True
+    return False
+
+
+def get_settings():
+    cmdParser = argparse.ArgumentParser()
+    cmdParser.add_argument("--config_fn", "-f", type=str, default="/etc/apache-log-exporter.yaml", help="name of config file")
+    args = cmdParser.parse_args(sys.argv[1:])
+
+    defaults = {
+        "input.filename": "apache-log-exporter.yaml",
+        "input.format": VHOST_COMBINED,
+        "input.ignoreExisting": "true",
+        "output.port": 9100,
+        "resolver": {"127.0.0.1": "localhost",
+                     "127.0.1.1": "localhost"},
+    }
+
+    with open(args.config_fn, "r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)    
+
+    settings = {
+        "fn": getParamDefault(cfg, "input.filename", defaults["input.filename"]),
+        "format": getParamDefault(cfg, "input.format", defaults["input.format"]),
+        "ignoreExisting": parseBool(getParamDefault(cfg, "input.ignoreExisting", defaults["input.ignoreExisting"])),
+        "port": int(getParamDefault(cfg, "output.port", defaults["output.port"])),
+        "resolver": getParamDefault(cfg, "resolver", defaults["resolver"]),
+    }
+
+    formatMap = {
+        "COMBINED": COMBINED,
+        "COMBINED_DEBIAN": COMBINED_DEBIAN,
+        "COMMON": COMMON,
+        "COMMON_DEBIAN": COMMON_DEBIAN,
+        "VHOST_COMBINED": VHOST_COMBINED,
+        "VHOST_COMMON": VHOST_COMMON
+    }
+
+    settings["format"] = formatMap.get(settings["format"], settings["format"])
+
+    return settings
+
+
 def main():
-    le = ApacheLogExporter()
+    if len(sys.argv) == 2:
+        config_fn = sys.argv[1]
+    else:
+        config_fn = "/etc/apache-log-exporter.yaml"
+
+    settings = get_settings()
+    print("settings: %s" % settings, file=sys.stderr)
+
+    le = ApacheLogExporter(**settings)
     le.read_log_files()
 
 
